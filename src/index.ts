6b90e4c6394e8ba469924c4f5e11dba3820a4f9e
Krystian Postek <krystian@postek.eu>
2021-11-26 20:32:35 +0100

    Add progressbar and replace event with wait expr
    and removed semi

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import puppeteer, { HTTPResponse } from 'puppeteer';
-import * as Process from 'process';
-import { JSDOM } from 'jsdom';
-import { multilinesIntoObject } from "./util";
-import { JsonDB } from 'node-json-db';
-import { Config } from "node-json-db/dist/lib/JsonDBConfig";
+import puppeteer, { HTTPResponse } from 'puppeteer'
+import * as Process from 'process'
+import { JSDOM } from 'jsdom'
+import { multilinesIntoObject } from './util'
+import { JsonDB } from 'node-json-db'
+import { Config } from 'node-json-db/dist/lib/JsonDBConfig'
+import { SingleBar } from 'cli-progress'
 
 const store = new JsonDB(new Config('store', true, true))
 
@@ -12,11 +13,11 @@ async function scrapDetails(event: HTTPResponse) {
     !event.ok() ||
     event.url() != 'https://planzajec.pjwstk.edu.pl/PlanOgolny3.aspx'
   )
-    return;
-  const responses = (await event.text()).split('|');
-  const fragment = JSDOM.fragment(responses[7]); // just 7, just csv things
+    return
+  const responses = (await event.text()).split('|')
+  const fragment = JSDOM.fragment(responses[7]) // just 7, just csv things
 
-  if (!fragment.textContent) return;
+  if (!fragment.textContent) return
   const content = fragment.textContent
     .replace(RegExp('(^\\s+$| {2,})', 'gm'), '')
     .replace(RegExp('\\n+', 'gm'), '\n')
@@ -27,24 +28,39 @@ async function scrapDetails(event: HTTPResponse) {
 async function main() {
   const browser = await puppeteer.launch({
     headless: false,
-  });
-  const page = await browser.newPage();
+  })
+  const page = await browser.newPage()
 
-  await page.goto('https://planzajec.pjwstk.edu.pl/PlanOgolny3.aspx');
+  await page.goto('https://planzajec.pjwstk.edu.pl/PlanOgolny3.aspx')
+  // set date
+  const datePicker = await page.$('#DataPicker_dateInput')
+  await datePicker?.click()
+  await datePicker?.press('Backspace')
+  await datePicker?.type('2021-11-25')
+  await datePicker?.press('Enter')
+  await page.waitForTimeout(2000)
+  // find all subjects
   // const subjects = await page.$x('//*[matches(@id, "\d+;r")]');
-  const subjects = await page.$x('//td[contains(@id, \';\')]');
+  const subjects = await page.$x("//td[contains(@id, ';')]")
 
-  await page.setRequestInterception(true);
-  page.on('request', async (event) => event.continue());
-  page.on('response', scrapDetails);
+  // enable request interception
+  await page.setRequestInterception(true)
+  page.on('request', async (event) => event.continue())
 
-  let progress = 0;
+  let progress = 0
+  const progressBar = new SingleBar({})
+  progressBar.start(subjects.length, 0)
 
   for (const subject of subjects) {
-    console.log(`${++progress}/${subjects.length}`);
-    await subject.hover();
-    await page.waitForTimeout(600);
+    await subject.hover()
+    const response = await page.waitForResponse(
+      'https://planzajec.pjwstk.edu.pl/PlanOgolny3.aspx'
+    )
+    await scrapDetails(response)
+    // console.log(`${++progress}/${subjects.length}`);
+    progressBar.update(++progress)
   }
+  progressBar.stop()
 }
 
-main().then(() => Process.exit());
+main().then(() => Process.exit())
